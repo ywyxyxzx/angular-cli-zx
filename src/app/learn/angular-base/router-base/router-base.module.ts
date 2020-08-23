9c96240fab1baa68eb9f3e0761e67515ed92d6a0
ywyxyxzx <ywyxyx@qq.com>
2020-08-23 23:12:49 +0800

    router1-1

--- src/app/learn/angular-base/router-base/router-base.module.ts
+++ src/app/learn/angular-base/router-base/router-base.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterBaseRouterModule } from './router-base-router.module'
+import { RouterBaseComponent } from './router-base.component'
 import { RouterModule, Routes } from '@angular/router';
+import { CrisisListComponent }   from './crisis-list/crisis-list.component';
+import { HeroListComponent }     from './hero-list/hero-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule,
-    RouterBaseRouterModule
+    RouterModule.forChild([
+      {
+        path: '',
+        component: RouterBaseComponent,
+        children: [
+
+          { path: 'crisis-center', component: CrisisListComponent },
+          { path: 'heroes', component: HeroListComponent },
+          { path: '**', component: PageNotFoundComponent }  
+        ]
+      }
+    ])
+
   ],
- 
+  declarations: [
+    RouterBaseComponent
+  ]
 })
 export class RouterBaseModule { }
