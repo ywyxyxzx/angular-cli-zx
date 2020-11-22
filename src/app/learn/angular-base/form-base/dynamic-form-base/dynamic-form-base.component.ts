ed4a46cde69cd93c43c940d76b9a555923d29cac
ywyxyxzx <ywyxyx@qq.com>
2020-11-22 22:11:10 +0800

    20201122.01

--- src/app/learn/angular-base/form-base/dynamic-form-base/dynamic-form-base.component.ts
+++ src/app/learn/angular-base/form-base/dynamic-form-base/dynamic-form-base.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { QuestionService } from './question.service';
+import { QuestionBase }    from './question-base';
+import { Observable }      from 'rxjs';
 @Component({
   selector: 'app-dynamic-form-base',
   templateUrl: './dynamic-form-base.component.html',
   styleUrls: ['./dynamic-form-base.component.scss']
 })
 export class DynamicFormBaseComponent implements OnInit {
-
-  constructor() { }
+  questions$: Observable<QuestionBase<any>[]>;
+  constructor(service: QuestionService) {
+    this.questions$ = service.getQuestions();
+  }
 
   ngOnInit() {
   }
--- src/app/learn/angular-base/form-base/dynamic-form-base/dynamic-form-base.module.ts
+++ src/app/learn/angular-base/form-base/dynamic-form-base/dynamic-form-base.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { DynamicFormBaseComponent } from './dynamic-form-base.component';
+import  {DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { DynamicFormQuestionComponent } from './dynamic-form-question/dynamic-form-question.component';
+import { QuestionService } from './question.service';
+// import { QuestionControlService } from './question-control.service';
 
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule, 
+    FormsModule,
     RouterModule.forChild([
       {
         path: '',
@@ -13,6 +20,14 @@ import { DynamicFormBaseComponent } from './dynamic-form-base.component';
       }
     ])
   ],
-  declarations: [DynamicFormBaseComponent]
+  declarations: [
+    DynamicFormBaseComponent,
+    DynamicFormComponent,
+    DynamicFormQuestionComponent
+  ],
+  providers:[
+    QuestionService,
+    // QuestionControlService
+  ]
 })
 export class DynamicFormBaseModule { }
